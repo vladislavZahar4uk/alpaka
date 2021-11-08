84ac852f7de33fd35aa9c83ee10db23613f3c9ca
Vladislav <grind_c@outlook.com>
2021-11-08 23:40:20 +0200

    Customize the assembly structure

--- webpack.config.js
+++ webpack.config.js
@@ -7,111 +7,124 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 function generateHtmlPlugins(templateDir) {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
-    const parts = item.split(".");
-    const name = parts[0];
-    const extension = parts[1];
-    return new HtmlWebpackPlugin({
-      filename: `${name}.html`,
-      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-      inject: false
-    });
-  });
+	const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+	return templateFiles.map(item => {
+		const parts = item.split(".");
+		const name = parts[0];
+		const extension = parts[1];
+		return new HtmlWebpackPlugin({
+			filename: `${name}.html`,
+			template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+			inject: false
+		});
+	});
 }
 
 const htmlPlugins = generateHtmlPlugins("./src/html/views");
 
 const config = {
-  entry: ["./src/js/app.js", "./src/scss/style.scss"],
-  output: {
-    filename: "./js/app.js"
-  },
-  devtool: "source-map",
-  mode: "production",
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        sourceMap: true,
-        extractComments: true
-      })
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, "src/scss"),
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {}
-          },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              url: false
-            }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              sourceMap: true,
-              plugins: () => [
-                require("cssnano")({
-                  preset: [
-                    "default",
-                    {
-                      discardComments: {
-                        removeAll: true
-                      }
-                    }
-                  ]
-                })
-              ]
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        include: path.resolve(__dirname, "src/html/includes"),
-        use: ["raw-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "./css/style.css"
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: "./src/fonts",
-        to: "./fonts"
-      },
-      {
-        from: "./src/favicon",
-        to: "./favicon"
-      },
-      {
-        from: "./src/img",
-        to: "./img"
-      }
-    ])
-  ].concat(htmlPlugins)
+	entry: {
+		'main': path.resolve(__dirname, './src/scss/main/style.scss'),
+		'internal': path.resolve(__dirname, './src/scss/internal/style.scss'),
+
+		'common': path.resolve(__dirname, './src/js/common.js'),
+
+		'user-area': path.resolve(__dirname, './src/js/user-area.js'),
+
+		'pages/index': path.resolve(__dirname, './src/js/pages/index.js'),
+		'pages/categories': path.resolve(__dirname, './src/js/pages/categories.js'),
+		'pages/product-cart': path.resolve(__dirname, './src/js/pages/product-cart.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "./js/[name].js"
+	},
+	devtool: "source-map",
+	mode: "production",
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				sourceMap: true,
+				extractComments: true
+			})
+		]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(sass|scss)$/,
+				include: path.resolve(__dirname, "src/scss"),
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {}
+					},
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true,
+							url: false
+						}
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							ident: "postcss",
+							sourceMap: true,
+							plugins: () => [
+								require("cssnano")({
+									preset: [
+										"default",
+										{
+											discardComments: {
+												removeAll: true
+											}
+										}
+									]
+								})
+							]
+						}
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.html$/,
+				include: path.resolve(__dirname, "src/html/includes"),
+				use: ["raw-loader"]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "./css/[name].css",
+			chunkFilename: "./css/[name].css"
+		}),
+		new CopyWebpackPlugin([
+			{
+				from: "./src/fonts",
+				to: "./fonts"
+			},
+			{
+				from: "./src/favicon",
+				to: "./favicon"
+			},
+			{
+				from: "./src/img",
+				to: "./img"
+			}
+		])
+	].concat(htmlPlugins)
 };
 
 module.exports = (env, argv) => {
-  if (argv.mode === "production") {
-    config.plugins.push(new CleanWebpackPlugin());
-  }
-  return config;
+	if (argv.mode === "production") {
+		config.plugins.push(new CleanWebpackPlugin());
+	}
+	return config;
 };
