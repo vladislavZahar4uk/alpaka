11bdafceac5dbf460e3b2fc843dbf0701c3ac574
Vladislav <grind_c@outlook.com>
2021-11-21 23:54:55 +0200

    Base footer layout

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -3,8 +3,9 @@ import 'slick-carousel';
 
 window.$ = $;
 
+
 $(function() {
-    $(".js-main-banner").slick({
+    $('.js-main-banner').slick({
         infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -12,4 +13,45 @@ $(function() {
         prevArrow: '<div class="main-slider__arrow left"> <span class="icon icon-arrow-left"></span> </div>',
         nextArrow: '<div class="main-slider__arrow right"> <span class="icon icon-arrow-right"></span> </div>'
     });
+
+    $('.js-products-new').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        appendArrows: $('.js-products-new-arrows'),
+        prevArrow: '<div class="slider-arrow left"> <span class="icon icon-arr-left"></span> </div>',
+        nextArrow: '<div class="slider-arrow right"> <span class="icon icon-arr-right"></span> </div>'
+    });
+
+    $('.js-categories-slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        appendArrows: $('.js-categories-arrows'),
+        prevArrow: '<div class="slider-arrow left"> <span class="icon icon-arr-left"></span> </div>',
+        nextArrow: '<div class="slider-arrow right"> <span class="icon icon-arr-right"></span> </div>'
+    });
+
+    $('.js-products-popular').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        appendArrows: $('.js-products-popular-arrows'),
+        prevArrow: '<div class="slider-arrow left"> <span class="icon icon-arr-left"></span> </div>',
+        nextArrow: '<div class="slider-arrow right"> <span class="icon icon-arr-right"></span> </div>'
+    });
+
+    $('.js-review-slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        appendArrows: $('.js-review-arrows'),
+        prevArrow: '<div class="slider-arrow left"> <span class="icon icon-arr-left"></span> </div>',
+        nextArrow: '<div class="slider-arrow right"> <span class="icon icon-arr-right"></span> </div>'
+    });
 });
+
